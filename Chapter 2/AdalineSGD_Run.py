f21108a1aa181e5166118b982f556eb23c377bcd
williamtylerbradley <william.tyler.bradley@gmail.com>
2016-05-25 20:56:17 -0400

    Finished the ADALINE section

--- Chapter 2/AdalineSGD_Run.py
+++ Chapter 2/AdalineSGD_Run.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import AdalineGD
+import AdalineSGD
 
 # Grab dataset
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
@@ -25,26 +25,11 @@ plt.ylabel('sepal length')
 plt.legend(loc='upper left')
 plt.show()
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-ada1 = AdalineGD.AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-ax[0].plot(range(1, len(ada1.cost_) + 1),
-           np.log10(ada1.cost_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - Learning rate 0.01')
-ada2 = AdalineGD.AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.cost_) + 1),
-           np.log10(ada2.cost_), marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('log(Sum-squared-error)')
-ax[1].set_title('Adaline - Learning rate 0.0001')
-plt.show()
-
 X_std = np.copy(X)
 X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
 X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
-ada = AdalineGD.AdalineGD(n_iter=15, eta=0.01)
+ada = AdalineSGD.AdalineSGD(n_iter=15, eta=0.01, random_state=1)
 ada.fit(X_std, y)
 
 from matplotlib.colors import ListedColormap
@@ -74,12 +59,12 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
                     marker=markers[idx], label=cl)
 
 plot_decision_regions(X_std, y, classifier=ada)
-plt.title('Adaline - Gradient Descent')
+plt.title('Adaline - Stochastic Gradient Descent')
 plt.xlabel('sepal length [standardized]')
 plt.ylabel('petal length [standardized]')
 plt.legend(loc='upper left')
 plt.show()
 plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
 plt.xlabel('Epochs')
-plt.ylabel('Sum-squared-error')
+plt.ylabel('Average Cost')
 plt.show()
